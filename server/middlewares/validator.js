cff1ff29ffec9c2579cf0edb41d5fa36613834da
Olatunbosun Adeoye <adex001@gmail.com>
2019-01-28 07:54:19 +0100

    ft #163496179 Wrote test for create political party endpoint Implemented create-party functionality

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -1,3 +1,5 @@
+import { types } from 'util';
+
 class Validator {
   static validateOffice(req, res, next) {
     const { name, type, description } = req.body;
@@ -20,6 +22,29 @@ class Validator {
       return res.json({
         status: 400,
         error: 'Please, enter a valid description! Description must be greater than 5 characters',
+      });
+    }
+    return next();
+  }
+
+  static validateParty(req, res, next) {
+    const { name, logo, address } = req.body;
+    if (typeof name !== 'string' || name.length < 3) {
+      return res.json({
+        status: 400,
+        error: 'Please, enter a valid name! Name must be greater than 3 characters',
+      });
+    }
+    if (typeof logo !== 'string' || logo.length < 5) {
+      return res.json({
+        status: 400,
+        error: 'Please, enter a valid logo URL!',
+      });
+    }
+    if (typeof address !== 'string' || address.length < 5) {
+      return res.json({
+        status: 400,
+        error: 'Please, enter a valid address and address must be greater than 5 characters',
       });
     }
     return next();
--- server/routes/party.js
+++ server/routes/party.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-cycle */
 import { Router } from 'express';
 import PartyController from '../controller/party';
+import Validator from '../middlewares/validator';
 
 const partyRouter = Router();
 
 partyRouter.get('/', PartyController.getAllParties);
 partyRouter.get('/:partyId', PartyController.getSpecificParty);
 partyRouter.delete('/:partyId', PartyController.deleteParty);
+partyRouter.post('/', Validator.validateParty, PartyController.createParty);
 
 export default partyRouter;
